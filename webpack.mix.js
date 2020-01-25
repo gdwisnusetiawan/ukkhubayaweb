d27f32fe11e6507258c500b27785e3fa6d85e939
Gede Wisnu Setiawan <gedewisnusetiawan@gmail.com>
2020-01-26 02:24:27 +0700

    copy adminlte assets to public

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,6 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+mix.copyDirectory('node_modules/admin-lte/dist', 'public/admin-lte/dist');
+mix.copyDirectory('node_modules/admin-lte/plugins', 'public/admin-lte/plugins');

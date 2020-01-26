1b109897e26567725ba5cc0eaf34144c022ae188
Gede Wisnu Setiawan <gedewisnusetiawan@gmail.com>
2020-01-26 17:26:21 +0700

    add meta tag csrf token

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,8 +8,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>UKKH UBAYA | @yield('title')</title>
+  <title>{{ config('app.name', 'UKKH UBAYA') }} | @yield('title')</title>
 
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="{{ asset('admin-lte/plugins/fontawesome-free/css/all.min.css') }}">

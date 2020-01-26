fbb1f34c52acb19216db9bb077e9012a82e45ec9
Gede Wisnu Setiawan <gedewisnusetiawan@gmail.com>
2020-01-27 05:50:43 +0700

    change ukkh ubaya copyright

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="main-footer">
   <!-- To the right -->
   <div class="float-right d-none d-sm-inline">
-    UKKH UBAYA &copy; 2020
+    &copy; 2020 UKKH UBAYA 
   </div>
   <!-- Default to the left -->
   <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.

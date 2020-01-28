15c3a4c9e73fc75b8f170d398f387fbfef330053
Gede Wisnu Setiawan <gedewisnusetiawan@gmail.com>
2020-01-28 23:18:29 +0700

    add href link route

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -70,7 +70,7 @@
               </a>
             </li>
             <li class="nav-item">
-              <a href="#" class="nav-link">
+              <a href="{{ route('faculties.index') }}" class="nav-link">
                 <i class="far fa-circle nav-icon"></i>
                 <p>Fakultas</p>
               </a>

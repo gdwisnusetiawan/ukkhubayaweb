6db052380e7205ff9bb982e7f6decaf7bb9778f3
Gede Wisnu Setiawan <gedewisnusetiawan@gmail.com>
2020-01-29 11:02:36 +0700

    change "anggota" nav item link

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -64,7 +64,7 @@
               </a>
             </li>
             <li class="nav-item">
-              <a href="#" class="nav-link">
+              <a href="{{ route('members.index') }}" class="nav-link">
                 <i class="far fa-circle nav-icon"></i>
                 <p>Anggota</p>
               </a>

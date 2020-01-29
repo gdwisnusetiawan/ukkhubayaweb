f388810de111feb9e4a05336b7da09a7fab77cf2
Gede Wisnu Setiawan <gedewisnusetiawan@gmail.com>
2020-01-29 11:25:15 +0700

    add function to retrieve enum values

--- app/Member.php
+++ app/Member.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model
@@ -19,4 +20,14 @@ class Member extends Model
      * @var array
      */
     protected $guarded = [];
+
+    public static function getEnumValues(){
+        $type = DB::select(DB::raw('SHOW COLUMNS FROM members WHERE Field = "type"'))[0]->Type;
+        preg_match('/^enum\((.*)\)$/', $type, $matches);
+        $values = array();
+        foreach(explode(',', $matches[1]) as $value){
+            $values[] = trim($value, "'");
+        }
+        return $values;
+    }
 }

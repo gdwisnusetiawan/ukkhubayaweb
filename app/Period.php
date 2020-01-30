8ae54a44b8749c688c45b4c582fcc8fa37648d44
Gede Wisnu Setiawan <gedewisnusetiawan@gmail.com>
2020-01-30 14:47:36 +0700

    set fillable year begin and year end

--- app/Period.php
+++ app/Period.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Period extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'year_begin', 'year_end',
+    ];
 }

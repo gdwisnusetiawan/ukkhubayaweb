c565a93ec8cf5a00fbac45d5520e4615c3db1367
Gede Wisnu Setiawan <gedewisnusetiawan@gmail.com>
2020-01-30 15:47:25 +0700

    fix form card width

--- resources/views/periods/create.blade.php
+++ resources/views/periods/create.blade.php
@@ -13,7 +13,7 @@
 
 @section('content')
 <div class="row">
-  <div class="col-md-8">
+  <div class="col-md-6">
     <div class="card">
       <h5 class="card-header">
       	Tambah Periode
@@ -23,8 +23,8 @@
 	        	@csrf
 
 	        	<div class="form-group row">
-	            <label for="year_begin" class="col-sm-2 col-form-label">Tahun Mulai</label>
-	            <div class="col-sm-10">
+	            <label for="year_begin" class="col-sm-4 col-form-label">Tahun Mulai</label>
+	            <div class="col-sm-8">
 	              <input type="text" class="form-control @error('year_begin') is-invalid @enderror" name="year_begin" value="{{ old('year_begin') }}" required autocomplete="year_begin" placeholder="Tahun Mulai">
 	              @error('year_begin')
 	                <span class="invalid-feedback" role="alert">
@@ -34,8 +34,8 @@
 	            </div>
 	          </div>
 	          <div class="form-group row">
-	            <label for="year_end" class="col-sm-2 col-form-label">Tahun Selesai</label>
-	            <div class="col-sm-10">
+	            <label for="year_end" class="col-sm-4 col-form-label">Tahun Selesai</label>
+	            <div class="col-sm-8">
 	              <input type="text" class="form-control @error('year_end') is-invalid @enderror" name="year_end" value="{{ old('year_end') }}" required autocomplete="year_end" placeholder="Tahun Selesai">
 	              @error('year_end')
 	                <span class="invalid-feedback" role="alert">

ae3cf63f05ea79afd85cb25646f997122f4dd1d8
Gede Wisnu Setiawan <gedewisnusetiawan@gmail.com>
2020-01-30 20:22:16 +0700

    make index function

--- app/Http/Controllers/PositionController.php
+++ app/Http/Controllers/PositionController.php
@@ -14,7 +14,8 @@ class PositionController extends Controller
      */
     public function index()
     {
-        //
+        $positions = Position::all();
+        return view('positions.index', compact('positions'));
     }
 
     /**

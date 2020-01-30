5ba2020fcee3727fcd86b71e213122c38891883c
Gede Wisnu Setiawan <gedewisnusetiawan@gmail.com>
2020-01-30 16:03:16 +0700

    create edit page

---
+++ resources/views/periods/edit.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.master')
+
+@push('css')
+@endpush
+
+@section('title', 'PERIODE')
+
+@section('breadcumb')
+  <li class="breadcrumb-item">Master</li>
+  <li class="breadcrumb-item"><a href="{{ route('periods.index') }}">Periode</a></li>
+  <li class="breadcrumb-item active">Ubah</li>
+@endsection
+
+@section('content')
+<div class="row">
+  <div class="col-md-6">
+    <div class="card">
+      <h5 class="card-header">
+      	Ubah Periode
+      </h5>
+      <div class="card-body">
+      	<form action="{{ route('periods.update', $period) }}" method="post" class="form-horizontal" id="form-edit">
+	        	@csrf
+	        	@method('put')
+
+	        	<div class="form-group row">
+	            <label for="year_begin" class="col-sm-4 col-form-label">Tahun Mulai</label>
+	            <div class="col-sm-8">
+	              <input type="text" class="form-control @error('year_begin') is-invalid @enderror" name="year_begin" value="{{ $period->year_begin }}" required autocomplete="year_begin" placeholder="Tahun Mulai">
+	              @error('year_begin')
+	                <span class="invalid-feedback" role="alert">
+	                  <strong>{{ $message }}</strong>
+	                </span>
+	              @enderror
+	            </div>
+	          </div>
+	          <div class="form-group row">
+	            <label for="year_end" class="col-sm-4 col-form-label">Tahun Selesai</label>
+	            <div class="col-sm-8">
+	              <input type="text" class="form-control @error('year_end') is-invalid @enderror" name="year_end" value="{{ $period->year_end }}" required autocomplete="year_end" placeholder="Tahun Selesai">
+	              @error('year_end')
+	                <span class="invalid-feedback" role="alert">
+	                  <strong>{{ $message }}</strong>
+	                </span>
+	              @enderror
+	            </div>
+	          </div>
+	        </form>
+	        <div class="float-right">
+	          <a href="{{ url()->current() == url()->previous() ? route('periods.index') : url()->previous() }}" type="button" class="btn btn-default">Kembali</a>
+	          <button type="submit" class="btn btn-primary" form="form-edit">Ubah</button>
+	        </div>
+      </div><!-- /.card-body -->
+    </div><!-- /.card -->
+  </div>
+  <!-- /.col-md-12 -->
+</div>
+<!-- /.row -->
+@endsection
+
+@push('js')
+@endpush

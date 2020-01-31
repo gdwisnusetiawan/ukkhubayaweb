c81d21ffae07b9b43771e42a74bd194efc8c9ea8
Gede Wisnu Setiawan <gedewisnusetiawan@gmail.com>
2020-01-31 23:54:57 +0700

    order by "order" field asc

--- app/Http/Controllers/PositionController.php
+++ app/Http/Controllers/PositionController.php
@@ -15,7 +15,7 @@ class PositionController extends Controller
      */
     public function index()
     {
-        $positions = Position::all();
+        $positions = Position::orderBy('order', 'asc')->get();
         return view('positions.index', compact('positions'));
     }
 

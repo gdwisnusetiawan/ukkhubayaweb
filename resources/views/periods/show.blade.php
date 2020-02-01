7e730f154914e7c82bdf6511d45989ff7c70f92e
Gede Wisnu Setiawan <gedewisnusetiawan@gmail.com>
2020-02-01 22:34:02 +0700

    change divisi card body layout

--- resources/views/periods/show.blade.php
+++ resources/views/periods/show.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.master')
 
 @push('css')
-  <!-- DataTables -->
-  <link rel="stylesheet" href="{{ asset('admin-lte/plugins/datatables-bs4/css/dataTables.bootstrap4.css') }}">
   <!-- SweetAlert2 -->
   <link rel="stylesheet" href="{{ asset('admin-lte/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css') }}">
 
@@ -35,7 +33,7 @@
     		<a href="{{ url()->current() == url()->previous() ? route('periods.index') : url()->previous() }}"><i class="fas fa-chevron-left"></i> Kembali</a>
     	</div>
       <div class="card-body box-profile">
-        <h3 class="profile-username text-center">{{ $period->name() }}</h3>
+        <h3 class="text-center">{{ $period->name() }}</h3>
         <ul class="list-group list-group-unbordered mb-3">
           <li class="list-group-item">
             <b>Anggota</b> <a class="float-right">0</a>
@@ -49,57 +47,53 @@
   <!-- /.col -->
   <div class="col-md-9">
     <div class="card">
-      <div class="card-header p-2">
-        <ul class="nav nav-pills">
-          <li class="nav-item"><a class="nav-link active" href="#divisi" data-toggle="tab">Divisi</a></li>
-        </ul>
-      </div><!-- /.card-header -->
+      <h5 class="card-header">
+        Daftar Divisi
+        <span class="float-right">
+          <a href="{{ route('managements.create') }}" class="btn btn-primary"><i class="fas fa-plus"></i><span class="button-text"> Tambah</span></a>
+        </span>
+      </h5>
       <div class="card-body">
-        <div class="tab-content">
-          <div class="active tab-pane" id="divisi">
-          	<div class="row">
-              @forelse ($managements as $management)
-              <div class="col-md-3 mb-3">
-                <div class="card h-100 m-0 text-center">
-                  <div class="card-body">
-                    <h1><i class="{{ $management->position->icon }}"></i></h1>
-                    <h2 class="card-text">{{ $management->position->name }}</h2>
-                  </div>
-                  <div class="card-footer">
-                    <a href="{{ route('managements.edit', $management) }}" type="button" class="btn btn-outline-primary btn-sm m-1"><i class="fas fa-edit"></i> Ubah</a>
-                    <button type="button" class="btn btn-outline-danger btn-sm m-1" data-toggle="modal" data-target="#modal-delete-{{ $management->id }}"><i class="fas fa-trash"></i> Hapus</button>
-                  </div>
-                </div>
+
+        <div class="row">
+          @forelse ($managements as $management)
+          <div class="col-md-3 mb-3">
+            <div class="card h-100 m-0 text-center">
+              <div class="card-body">
+                <h1><i class="{{ $management->position->icon }}"></i></h1>
+                <h2 class="card-text">{{ $management->position->name }}</h2>
               </div>
-              @empty
-              <p class="text-center w-100">Tidak ada data.</p>
-              @endforelse
-
-              @foreach ($managements as $management)
-              <!-- Modal Delete -->
-                @component('components.modal')
-                  @slot('id') modal-delete-{{ $management->id }} @endslot
-                  @slot('title') Hapus Divisi @endslot
-                  @slot('button_type') danger @endslot
-                  @slot('button_name') Hapus @endslot
-                  @slot('form_id') form-delete-{{ $management->id }} @endslot
-
-                  <p>Apakah Anda yakin ingin menghapus data <strong>{{ $management->position->name }}</strong> pada <strong>{{ $period->name() }}</strong>?</p>
-                  <form action="{{ route('managements.destroy', $management) }}" method="post" id="form-delete-{{ $management->id }}">
-                    @csrf
-                    @method('delete')
-                  </form>
-                @endcomponent
-                <!-- /.modal -->
-              @endforeach
+              <div class="card-footer">
+                <a href="{{ route('managements.edit', $management) }}" type="button" class="btn btn-outline-primary btn-sm m-1"><i class="fas fa-edit"></i> Ubah</a>
+                <button type="button" class="btn btn-outline-danger btn-sm m-1" data-toggle="modal" data-target="#modal-delete-{{ $management->id }}"><i class="fas fa-trash"></i> Hapus</button>
+              </div>
             </div>
           </div>
-          <!-- /.tab-pane -->
+          @empty
+          <p class="text-center w-100">Tidak ada data.</p>
+          @endforelse
+
+          @foreach ($managements as $management)
+          <!-- Modal Delete -->
+            @component('components.modal')
+              @slot('id') modal-delete-{{ $management->id }} @endslot
+              @slot('title') Hapus Divisi @endslot
+              @slot('button_type') danger @endslot
+              @slot('button_name') Hapus @endslot
+              @slot('form_id') form-delete-{{ $management->id }} @endslot
+
+              <p>Apakah Anda yakin ingin menghapus data <strong>{{ $management->name }}</strong>?</p>
+              <form action="{{ route('managements.destroy', $management) }}" method="post" id="form-delete-{{ $management->id }}">
+                @csrf
+                @method('delete')
+              </form>
+            @endcomponent
+            <!-- /.modal -->
+          @endforeach
         </div>
-        <!-- /.tab-content -->
+
       </div><!-- /.card-body -->
-    </div>
-    <!-- /.nav-tabs-custom -->
+    </div><!-- /.card -->
   </div>
   <!-- /.col -->
 </div>

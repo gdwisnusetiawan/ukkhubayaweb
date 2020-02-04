26211baee694bf10577409ea6c1be77d2f21450b
Gede Wisnu Setiawan <gedewisnusetiawan@gmail.com>
2020-02-04 11:08:07 +0700

    fix management index page showing member of the management in hthe period chosen

--- resources/views/managements/index.blade.php
+++ resources/views/managements/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.master')
 
 @push('css')
+  <!-- Select2 -->
+  <link rel="stylesheet" href="{{ asset('admin-lte/plugins/select2/css/select2.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('admin-lte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
   <!-- SweetAlert2 -->
   <link rel="stylesheet" href="{{ asset('admin-lte/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css') }}">
 
@@ -28,48 +31,77 @@
   <div class="col-lg-12">
     <div class="card">
       <h5 class="card-header">
-	      Daftar Pengurus
-	      <span class="float-right">
-		      <a href="{{ route('managements.create') }}" class="btn btn-primary"><i class="fas fa-plus"></i><span class="button-text"> Tambah</span></a>
-	      </span>
+      	<div class="d-flex justify-content-between">
+      		<span>
+    	    <label for="period_id" class="col-form-label">Daftar Pengurus</label>
+		      <select class="select2bs4" name="period_id" required>
+	        	@foreach ($periods as $period)
+	        		<option value="{{ $period->id }}" {{ $periodLast->id == $period->id ? 'selected' : '' }}>Periode {{ $period->name() }}</option>
+	        	@endforeach
+	        </select>
+	        </span>
+		      <span class="float-right">
+			      <a href="{{ route('managements.create') }}" class="btn btn-primary"><i class="fas fa-plus"></i><span class="button-text"> Tambah</span></a>
+		      </span>
+	      </div>
 	  </h5>
       <div class="card-body">
 
+  	    <div class="row d-flex align-items-stretch">
+  	    	@forelse ($managements as $management)
+  	    		@forelse ($management->members as $member)
+			      <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch">
+			        <div class="card bg-light">
+			          <div class="card-header text-muted border-bottom-0">
+			            
+			          </div>
+			          <div class="card-body pt-0">
+			            <div class="row">
+			            	<div class="col-5 text-center">
+			              	<img class="img-fluid rounded"
+			              		@if ($member->type == 'student')
+			              	    src="https://my.ubaya.ac.id/img/mhs/{{ $member->id }}_m.jpg"
+			              		@else
+			              			src="https://my.ubaya.ac.id/img/krwyn/{{ $member->id }}_m.jpg"
+			              		@endif
+			              	    alt="User profile picture">
+			              </div>
+			              <div class="col-7">
+			              	<h4 class="text-muted"><i class="{{ $management->position->icon }}"></i> {{ $management->position->name }}</h4>
+			              	@if (strlen($member->name) > 30)
+			              		<h5><b>{{ $member->name }}</b></h5>
+			              	@else
+			              		<h3><b>{{ $member->name }}</b></h3>
+			              	@endif
+			              </div>
+			            </div>
+			          </div>
+			          <div class="card-footer">
+			            <div class="text-right">
+  	      	      	<a href="{{ route('members.show', $member->id) }}" type="button" class="btn btn-outline-primary btn-sm m-1"><i class="fas fa-eye"></i></a>
+  	      	      	<a href="{{ route('managements.edit', $management) }}" type="button" class="btn btn-outline-primary btn-sm m-1"><i class="fas fa-edit"></i></a>
+  		      	      <button type="button" class="btn btn-outline-danger btn-sm m-1" data-toggle="modal" data-target="#modal-delete-{{ $management->id }}"><i class="fas fa-trash"></i></button>
+			            </div>
+			          </div>
+			        </div>
+			      </div>
+			      @empty
+			      <p></p>
+			      @endforelse
+			    @empty
+			    <p></p>
+			    @endforelse
+  	    </div>
+  	    <!-- /.row -->
+
       	<div class="row">
       		@forelse ($managements as $management)
+      		<!-- I don't know why, but this is making it not flickering when loading -->
       	  <div class="col-md-3 mb-3">
-      	    <div class="card h-100 m-0 text-center">
-      	      <div class="card-body">
-      	      	<h1><i class="{{ $management->position->icon }}"></i></h1>
-	      	      <h2 class="card-text">{{ $management->position->name }}</h2>
-      	      </div>
-      	      <div class="card-footer">
-      	      	<a href="{{ route('managements.edit', $management) }}" type="button" class="btn btn-outline-primary btn-sm m-1"><i class="fas fa-edit"></i> Ubah</a>
-	      	        <button type="button" class="btn btn-outline-danger btn-sm m-1" data-toggle="modal" data-target="#modal-delete-{{ $management->id }}"><i class="fas fa-trash"></i> Hapus</button>
-      	      </div>
-      	    </div>
       	  </div>
       	  @empty
-      	  <p class="text-center w-100">Tidak ada data.</p>
+      	  <p class="text-center w-100"></p>
 	      	@endforelse
-
-	      	@foreach ($managements as $management)
-	      	<!-- Modal Delete -->
-		      	@component('components.modal')
-		      		@slot('id') modal-delete-{{ $management->id }} @endslot
-		      		@slot('title') Hapus Pengurus @endslot
-		      		@slot('button_type') danger @endslot
-		      		@slot('button_name') Hapus @endslot
-		      		@slot('form_id') form-delete-{{ $management->id }} @endslot
-
-		      		<p>Apakah Anda yakin ingin menghapus data <strong>{{ $management->name }}</strong>?</p>
-		      		<form action="{{ route('managements.destroy', $management) }}" method="post" id="form-delete-{{ $management->id }}">
-		      			@csrf
-		      			@method('delete')
-		      		</form>
-		      	@endcomponent
-		      	<!-- /.modal -->
-		    	@endforeach
       	</div>
 
       </div><!-- /.card-body -->
@@ -81,11 +113,19 @@
 @endsection
 
 @push('js')
+	<!-- Select2 -->
+	<script src="{{ asset('admin-lte/plugins/select2/js/select2.full.min.js') }}"></script>
 	<!-- SweetAlert2 -->
 	<script src="{{ asset('admin-lte/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
 	<!-- page script -->
 	<script>
 	  $(function () {
+	  	//Initialize Select2 Elements
+	    $('.select2bs4').select2({
+	      theme: 'bootstrap4',
+	      width: 'auto',
+	    });
+
 	    $("#example1").DataTable();
 	    $('#example2').DataTable({
 	      "paging": true,

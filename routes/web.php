724b67538893e722b868b1098622ea42858b8716
Gede Wisnu Setiawan <gedewisnusetiawan@gmail.com>
2020-02-15 01:34:03 +0700

    add committees create route with event parameter

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,5 @@ Route::resource('committees', 'CommitteeController');
 
 Route::get('managements/create/{period}', 'ManagementController@create')->name('managements.createPeriod');
 Route::get('managements/{management}/edit/{period}', 'ManagementController@edit')->name('managements.editPeriod');
+
+Route::get('committees/create/{event}', 'CommitteeController@create')->name('committees.createEvent');

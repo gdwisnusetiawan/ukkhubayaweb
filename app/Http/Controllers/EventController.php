e8eddb3c16465d7fa1b307ace32d2d0db924304f
Gede Wisnu Setiawan <gedewisnusetiawan@gmail.com>
2020-02-15 00:40:34 +0700

    create event destroy function

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -147,6 +147,8 @@ class EventController extends Controller
      */
     public function destroy(Event $event)
     {
-        //
+        $event->delete();
+
+        return redirect('events')->with('status', 'Sukses menghapus data.');
     }
 }

555c9469a60642444a8429a4a1e37677378ff7ac
Gede Wisnu Setiawan <gedewisnusetiawan@gmail.com>
2020-02-16 15:23:52 +0700

    overlap committee edit route adding event parameter

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,4 @@ Route::get('managements/create/{period}', 'ManagementController@create')->name('
 Route::get('managements/{management}/edit/{period}', 'ManagementController@edit')->name('managements.editPeriod');
 
 Route::get('committees/create/{event}', 'CommitteeController@create')->name('committees.createEvent');
+Route::get('committees/{committee}/edit/{event}', 'CommitteeController@edit')->name('committees.editEvent');

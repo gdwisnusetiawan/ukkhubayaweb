1d6bdf8a4f602aaf9e2d5d28fc93cfb5e70b6176
Gede Wisnu Setiawan <gedewisnusetiawan@gmail.com>
2020-02-18 02:29:34 +0700

    make profile model with migration, controller, and resources

---
+++ database/migrations/2020_02_17_192639_create_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('logo');
+            $table->longtext('vision');
+            $table->longtext('mision');
+            $table->longtext('history');
+            $table->date('established');
+            $table->longtext('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}

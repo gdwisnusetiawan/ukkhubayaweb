2dd347b5573d4158a65dc6ad76af373d810b4637
Gede Wisnu Setiawan <gedewisnusetiawan@gmail.com>
2020-02-18 04:09:46 +0700

    add profiles create store function

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Profile;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
 {
@@ -36,7 +37,43 @@ class ProfileController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'name' => 'required|max:255|unique:profiles',
+            'logo' => 'required|file|image|mimes:jpeg,png|max:1000',
+            'established' => 'required|date|date_format:Y-m-d',
+            'address' => 'required',
+            'vision' => 'nullable',
+            'mission' => 'nullable',
+            'history' => 'nullable',
+            'description' => 'nullable',
+        ]);
+
+        $count = Profile::all()->count();
+        if($count <= 0)
+        {
+            $file = $request->file('logo');
+            $name = $request->get('name');
+            $name = $name.' Logo.'.$file->getClientOriginalExtension();
+            $file->move('images/logos/', $name);
+            // $path = $request->file('logo')->store('logos');
+
+            $profile = new Profile();
+            $profile->name = $request->get('name');
+            $profile->logo = 'images/logos/'.$name;
+            $profile->established = $request->get('established');
+            $profile->address = $request->get('address');
+            $profile->vision = $request->get('vision');
+            $profile->mission = $request->get('mission');
+            $profile->history = $request->get('history');
+            $profile->description = $request->get('description');
+            $profile->save();
+
+            return redirect('profiles')->with('status', 'Sukses menambah data.');
+        }
+        else
+        {
+            return redirect('profiles')->with('error', 'Gagal menambah data. Hanya boleh mengisi 1 data profile. Silahkan ubah data profile yang ada.');
+        }
     }
 
     /**

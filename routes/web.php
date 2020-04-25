8d97d22728d7869b2c47a4d1906ff1e78f654059
Gede Wisnu Setiawan <gedewisnusetiawan@gmail.com>
2020-04-25 07:23:47 +0700

    implement must verified

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return redirect()->route('home');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('storage/{filename}', 'HomeController@displayImage')->name('storage.displayImage');

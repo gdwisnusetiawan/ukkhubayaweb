1e2cc893fbdd6f17d87043224ff707d12921c372
Gede Wisnu Setiawan <gedewisnusetiawan@gmail.com>
2020-04-26 10:22:54 +0700

    add fillable id

--- app/User.php
+++ app/User.php
@@ -23,7 +23,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'id', 'name', 'email', 'password',
     ];
 
     /**

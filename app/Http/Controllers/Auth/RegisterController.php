81751f5343e14254d770f4b7124ae60e48ce4b57
Gede Wisnu Setiawan <gedewisnusetiawan@gmail.com>
2020-04-26 10:24:33 +0700

    fix registering member

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\User;
+use App\Member;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -50,8 +51,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'id' => ['required', 'string', 'max:9', 'unique:users', 'exists:members'],
+            // 'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users', 'ends_with:ubaya.ac.id'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
@@ -64,10 +66,16 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
+        $member = Member::find($data['id']);
+        $user = User::create([
+            'id' => $data['id'],
+            'name' => $member->name,
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'role' => 'viewer',
         ]);
+        $member->user()->associate($user->id);
+        $member->save();
+        return $user;
     }
 }
